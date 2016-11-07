33855913c778024f56aecdcab34a5e60d89302fa
Robin Dupret <robin.dupret@gmail.com>
2016-11-07 16:58:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct {
 	unsigned int ligne[3];
@@ -11,40 +12,36 @@ typedef struct {
 	region regions[9];
 } grille;
 
-FILE *chargerFichier(char *f)
-{
-	FILE *fic= NULL;
-	
-	fic = fopen(f, "r");
+int nombreLignes(char *nom_fichier) {
+  FILE *fichier = fopen(nom_fichier, "r");
 
-	if(fic != NULL)
-		return fic;
-	else
-		return NULL;
-}
-
-int nombreLignes(FILE *f)
-{
 	char c;
 	int nbLignes = 0;
-	while((c = fgetc(f)) != EOF)
-	{
-		if(c == '\n')
+
+	while ((c = fgetc(fichier)) != EOF) {
+		if (c == '\n')
 			nbLignes++;
 	}
+
+  fclose(fichier);
+
 	return nbLignes;
 }
 
-int *recupererChaine(FILE *f)
-{
+int *recupererGrille(char *nom_fichier) {
+  FILE *fichier = fopen(nom_fichier, "r");
+
 	int d, n = 0;
-	int *chaine = (int*)malloc(81 * sizeof(int));
-    while (fscanf(f, "%1d", &d) == 1 && n < 81)
-    {
-	   	chaine[n] = d;
-	   	n++;
-    } 
-    return chaine;
+	int *grille = (int*)malloc(81 * sizeof(int));
+
+  while (fscanf(fichier, "%1d", &d) == 1) {
+   	grille[n] = d;
+   	n++;
+  }
+
+  fclose(fichier);
+
+  return grille;
 }
 
 void afficherGrille(int *grille) {
@@ -52,8 +49,8 @@ void afficherGrille(int *grille) {
 	printf("++---+---+---++\n");
 	do
     {
-        printf("%d - ", i); 
-        printf("%d \n", grille[i]); 
+        printf("%d - ", i);
+        printf("%d \n", grille[i]);
         i++;
     } while (i < 81);
     printf("++---+---+---++\n");
@@ -61,26 +58,88 @@ void afficherGrille(int *grille) {
 }
 
 
-FILE *solve (char *grille)
+void solve (char *grille)
 {
 	return;
 }
 
-void main (int argc, char **argv)
-{
-	int i;
+void verifierFicher() {
+  // Checker si c'est la bonne taille
+  // Si elle est valide de base
+  //   - Si c'est les bons caractères
+  //   - Si les caractères sont bien placés (pas deux fois le même
+  //     sur la même ligne ou la même colonne.
+}
 
-	FILE *fichier = chargerFichier("grilles.txt");
-	int nbLignes = nombreLignes(fichier);
-	printf("Nombres de grilles dans le fichier : %d\n", nbLignes);
+/**
+ * Affiche une erreur passé en paramètre et sort du programme
+ * avec un code d'erreur 1.
+ */
+void erreur(const char* message) {
+  printf("Erreur : %s", message);
+  exit(1);
+}
 
+void afficherOptions(char *name) {
+  printf("Utilisation: %s [fichier] [options]", name);
 
-	int *tab = (int *)malloc(nbLignes * sizeof(int));
+  printf("\n\n");
 
-	for(i = 0; i < nbLignes; i++)
-	{
-		tab = recupererChaine(fichier);
-		afficherGrille(tab);
-	}
-	return;
+  printf("--help:      Affiche cette aide.\n");
+  printf("--verbose:   Solve en détaillant les operations.\n");
+  printf("--timeAlert: Temps en millisecondes pour arrêter la resolution\n");
+  printf("             de la grille si le temps donne est depasse.\n");
+}
+
+/**
+ * Fonction qui vérifie si une option a été passée en paramètre
+ * en ligne de commande.
+ */
+bool option(int argc, char** argv, char* option) {
+  for (int i = 0; i < argc; i++)
+    if (strcmp(argv[i], option) == 0)
+      return true;
+
+  return false;
+}
+
+/**
+ * Fonction qui récupère la valeur passé en paramètre
+ * de l'otpion 'timeAlert'.
+ */
+void timeAlert(int argc, char** argv, float *valeur) {
+  int i;
+
+  for (i = 0; i < argc; i++)
+    if (strcmp(argv[i], "--timeAlert") == 0)
+      break;
+
+  *valeur = strtof(argv[i+1], NULL);
+}
+
+int main(int argc, char** argv) {
+  int i;
+  bool  verbose;
+  float time_alert;
+  char nom_fichier[] = "grilles.txt";
+
+  int nbLignes = nombreLignes(nom_fichier);
+  printf("Nombres de grilles dans le fichier : %d\n", nbLignes);
+
+  if (argc == 1 || option(argc, argv, "--help"))
+    afficherOptions(argv[0]);
+
+  verbose = option(argc, argv, "--verbose");
+
+  if (option(argc, argv, "--timeAlert"))
+    timeAlert(argc, argv, &time_alert);
+
+  int *tab = (int *)malloc(nbLignes * sizeof(int));
+
+  for (i = 0; i < nbLignes; i++) {
+    tab = recupererGrille(nom_fichier);
+    afficherGrille(tab);
+  }
+
+  return 0;
 }
